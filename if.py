275c16082c975405096fb13b810238ce26423bd7
Laura Miller <lauramiller.spence@gmail.com>
2020-06-19 14:13:15 +0100

    Adding elif

--- if.py
+++ if.py
@@ -2,5 +2,7 @@ day_of_week = input("What day of the week is it today? ")
 
 if day_of_week == "Monday":
     print("Have a great start to your week!")
+elif day_of_week == "Tuesday":
+    print("Hang tight, it's almost the weekend! Sort of...")
 else:
     print("Thank God. I hate Mondays.")
