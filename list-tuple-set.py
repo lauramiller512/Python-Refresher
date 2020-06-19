442a9e2ea15d81cf663ed45f811967152010c2a7
Laura Miller <lauramiller.spence@gmail.com>
2020-06-19 13:50:51 +0100

    Using union method to return total from two sets

--- list-tuple-set.py
+++ list-tuple-set.py
@@ -1,7 +1,7 @@
-friends = {"Olivia", "Jyoti", "Amelia"}
+local = {"Jyoti"}
 abroad = {"Olivia", "Amelia"}
 
-local_friends = friends.difference(abroad) # looks within friends to find the duplicates from abroad
-print(local_friends) # prints {"Jyoti"}
+# z = x.difference(y)  returns set of items that only exist in set x, and not in set y
 
-z = x.difference(y) # returns set of items that only exist in set x, and not in set y
+friends = local.union(abroad) # unites both sets to give total 
+print(friends)
