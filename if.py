2afa72f39ad827ed0ea9578ec6b3111bd9844202
Laura Miller <lauramiller.spence@gmail.com>
2020-06-19 14:10:30 +0100

    Converted it into an if/else statement

--- if.py
+++ if.py
@@ -2,6 +2,5 @@ day_of_week = input("What day of the week is it today? ")
 
 if day_of_week == "Monday":
     print("Have a great start to your week!")
-
-if day_of_week != "Monday":
+else:
     print("Thank God. I hate Mondays.")
