ccd8f579a9f68bee54431b14d32246e654f179d4
Laura Miller <lauramiller.spence@gmail.com>
2020-06-19 13:38:45 +0100

    Appending item to the list

--- list-tuple-set.py
+++ list-tuple-set.py
@@ -1,6 +1,9 @@
 l = ["Bob", "Laura", "Pearce"]
-t = ("Bob", "Laura", "Pearce")
+t = ("Bob", "Laura", "Pearce") # tuples cannot be modified after they are created
 s = {"Bob", "Laura", "Pearce"} # items in a set are not in order
 
 l[0] = "Louie"
+print(l)
+
+l.append("Ozzy")
 print(l)
