dbe0fbe6ca8e65677ef8e4a9ce92d4a807aa9990
Laura Miller <lauramiller.spence@gmail.com>
2020-06-19 13:37:06 +0100

    Changing specific item in the list

--- list-tuple-set.py
+++ list-tuple-set.py
@@ -1,5 +1,6 @@
 l = ["Bob", "Laura", "Pearce"]
 t = ("Bob", "Laura", "Pearce")
-s = {"Bob", "Laura", "Pearce"}
+s = {"Bob", "Laura", "Pearce"} # items in a set are not in order
 
-print(l[0])
+l[0] = "Louie"
+print(l)
